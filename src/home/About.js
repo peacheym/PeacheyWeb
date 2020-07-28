29f41825cf9a0685b0f6d1520e822984e27cb41a
Matt Peachey <peacheym@dal.ca>
2020-07-28 12:57:57 -0300

    Update about component to reflect my actual school year.

--- src/home/About.js
+++ src/home/About.js
@@ -12,8 +12,8 @@ class About extends Component {
             </Col>
             <Col md>
                 <h2>About Me</h2>
-                <p>I am a third year student at Dalhousie University in Halifax, Nova Scotia. I am currently studying Computer Science and have a particular interest in the Internet of Things (IOT). I am also interested in learning more about Machine Learning and Artificial Intelligence as well as software development as a whole.</p>
-                <p>Through my studies I have developed a strong understanding of the fundamentals of Computer Science such as data structures and algorithims, as well as object oriented programming techniques. I have also recently enjoyed learning about web development and the interesting frameworks and technologies built around it.</p>
+                <p>I am a fourth year student at Dalhousie University in Halifax, Nova Scotia. I am currently studying Computer Science and have a particular interest in Computer Vision as well as the Internet of Things (IOT).</p>
+                <p>Through my studies I have developed a strong understanding of the fundamentals of Computer Science such as data structures, algorithms, and machine learning. I have also recently enjoyed learning about web development and the interesting frameworks and technologies built around it.</p>
             </Col>
         </Row>
       </Container>
