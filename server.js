b6863bd735c45c8abefadd5850aadf216006c487
Matt Peachey <peacheym@dal.ca>
2020-09-15 13:06:38 -0300

    Add basic graphql endpoint to express app.

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-var express = require("express")
-const path = require('path');
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+var express = require("express");
+const path = require("path");
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Server is running");
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port: 3000");
 });
