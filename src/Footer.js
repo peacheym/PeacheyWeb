db276a6dc18d1144392322cd69dc868fc13c1930
Matt Peachey <peacheym@dal.ca>
2021-01-20 23:21:33 -0400

    Update footer date. TODO: Make this automatic via JS.

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import './App.css';
 
 class Footer extends Component {
   render() {
-      return <footer>&copy; 2020 - Matt Peachey</footer>
+      return <footer>&copy; 2021 - Matt Peachey</footer>
   }
 }
 export default Footer;
