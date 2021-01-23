b9d9d4421803827fa8aa37ce687ef7f616d54965
Matt Peachey <peacheym@dal.ca>
2021-01-23 13:53:06 -0400

    Finish updating all components to functional.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import { Navbar, Nav } from "react-bootstrap";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Matt Peachey</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/research">Research</Nav.Link>
-            <Nav.Link href="/experience">Work</Nav.Link>
-            <Nav.Link href="/volunteer">Volunteer</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+export default function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Matt Peachey</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/research">Research</Nav.Link>
+          <Nav.Link href="/experience">Work</Nav.Link>
+          <Nav.Link href="/volunteer">Volunteer</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
-export default NavBar; // Don’t forget to use export default!
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,32 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 // import FeaturedProjects from "../FeaturedProjects";
 import About from "./About";
 import Contact from "./Contact";
 import { Jumbotron, Container } from "react-bootstrap";
 import "../App.css";
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Jumbotron fluid className="jt">
-          <Container>
-            <h1>Matt Peachey</h1>
-            <p>
-              Fourth year Computer Science student from Dalhousie University!
-            </p>
-            {/* <Button className="btn-dark" href="#about">
-              About Me
-            </Button> */}
-          </Container>
-        </Jumbotron>
-        <About />
-        <Contact />
-        <br /> <br />
-      </div>
-    );
-  }
+export default function Home() {
+  return (
+    <div>
+      <Jumbotron fluid className="jt">
+        <Container>
+          <h1>Matt Peachey</h1>
+          <p>Fourth year Computer Science student from Dalhousie University!</p>
+        </Container>
+      </Jumbotron>
+      <About />
+      <Contact />
+      <br /> <br />
+    </div>
+  );
 }
-export default Home; // Don’t forget to use export default!
 
 //TODO: Insert FeaturedProjects back into return
