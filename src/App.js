03cc9f027785f3c873c05332f36b8c07516a95bc
Matt Peachey <peacheym@dal.ca>
2021-05-07 09:12:48 -0300

    Continue development of components. TODO: Update responsive design.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 
-// import Header from "./Header";
-// import About from "./About";
-// import Research from "./Research";
-// import Experience from "./Experience";
-// import Footer from "./Footer";
+import Header from "./Header";
+import About from "./About";
+import Research from "./Research";
+import Experience from "./Experience";
+import Footer from "./Footer";
 import Construction from "./Construction";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      {/* <Header />
+      <Header />
       <About />
       <Research />
       <Experience />
-      <Footer /> */}
-      <Construction />
+      <Footer />
+      {/* <Construction /> */}
     </ChakraProvider>
   );
 }
