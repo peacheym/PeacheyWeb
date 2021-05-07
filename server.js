13e7df6caf5bc3b100c285fcd29f163de981969d
Matt Peachey <peacheym@dal.ca>
2021-05-07 07:37:43 -0300

    Update server.js to handle new directory structure.

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-var { graphqlHTTP } = require("express-graphql");
-var { buildSchema } = require("graphql");
+// var { graphqlHTTP } = require("express-graphql");
+// var { buildSchema } = require("graphql");
 const cors = require("cors");
 var express = require("express");
 const path = require("path");
-const gqlSchema = require("./schema");
-const gqlResolvers = require("./resolvers");
+// const gqlSchema = require("./schema");
+// const gqlResolvers = require("./resolvers");
 
 var app = express();
 app.use(cors());
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: gqlSchema,
-    rootValue: gqlResolvers,
-    graphiql: true,
-  })
-);
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema: gqlSchema,
+//     rootValue: gqlResolvers,
+//     graphiql: true,
+//   })
+// );
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "/build")));
 
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 app.listen(process.env.PORT || 3000, function () {
