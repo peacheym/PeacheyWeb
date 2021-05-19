a5865673058eb219fdf5f20724f6eb112aeb1e74
Matt Peachey <peacheym@dal.ca>
2021-05-18 21:29:48 -0300

    Clean server.js

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-// var { graphqlHTTP } = require("express-graphql");
-// var { buildSchema } = require("graphql");
 const cors = require("cors");
 var express = require("express");
 const path = require("path");
-// const gqlSchema = require("./schema");
-// const gqlResolvers = require("./resolvers");
 
 var app = express();
 app.use(cors());
-
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema: gqlSchema,
-//     rootValue: gqlResolvers,
-//     graphiql: true,
-//   })
-// );
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "/build")));
