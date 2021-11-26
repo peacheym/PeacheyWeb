e590177b4a4a6b4f02e09850528266cd92c1279d
Matt Peachey <peacheym@dal.ca>
2021-11-26 09:30:48 -0400

    Update NavBar to use new component.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 
-import Header from "./Header";
 import About from "./About";
 import Research from "./Research";
 import Experience from "./Experience";
 import Footer from "./Footer";
 import Construction from "./Construction";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
+      <NavBar />
       <About />
       <Research />
       <Experience />
