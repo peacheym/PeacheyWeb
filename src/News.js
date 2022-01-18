ec5334b216d02b33d3d5135b55a9bbf7f5931654
Matt Peachey <peacheym@dal.ca>
2022-01-18 08:09:13 -0400

    Update font size for NEWS.

--- src/News.js
+++ src/News.js
@@ -24,7 +24,7 @@ export default function Timeline() {
       <Heading
         as="h2"
         px={{ base: "10px", lg: 0 }}
-        fontSize={{ base: "22px", md: "36px" }}
+        fontSize={{ base: "20px", md: "36px" }}
         textAlign={{ base: "center", md: "left" }}
       >
         In the News
@@ -34,13 +34,13 @@ export default function Timeline() {
         <ListItem>
           <ListIcon
             as={ExternalLinkIcon}
-            fontSize={"22px"}
+            fontSize={"20px"}
             color="orange.500"
           />
-          <Text fontSize={"20px"} display={"inline"}>
+          <Text fontSize={"18px"} display={"inline"}>
             <Link
               isExternal
-              fontSize={"22px"}
+              fontSize={"20px"}
               href="https://www.cbc.ca/news/canada/nova-scotia/students-float-creative-ideas-for-future-bedford-ferry-1.6244736"
             >
               Students float creative ideas for future Bedford ferry
@@ -52,13 +52,13 @@ export default function Timeline() {
           {" "}
           <ListIcon
             as={ExternalLinkIcon}
-            fontSize={"22px"}
+            fontSize={"20px"}
             color="orange.500"
           />
-          <Text fontSize={"20px"} display={"inline"}>
+          <Text fontSize={"18px"} display={"inline"}>
             <Link
               isExternal
-              fontSize={"22px"}
+              fontSize={"20px"}
               href="https://www.dal.ca/faculty/computerscience/news-events/news/2020/11/02/new_covid_19_screening_app_for_health_students_born_from_necessity_and_collaboration.html?fbclid=IwAR2r8ygLq_woBwngySTSSqMwAmBrIC24Rdrjb4eP5Vz6xwYP6B9dNbZm5qI"
             >
               New COVID‑19 screening app for Health students born from necessity
@@ -71,13 +71,13 @@ export default function Timeline() {
           {" "}
           <ListIcon
             as={ExternalLinkIcon}
-            fontSize={"22px"}
+            fontSize={"20px"}
             color="orange.500"
           />
-          <Text fontSize={"20px"} display={"inline"}>
+          <Text fontSize={"18px"} display={"inline"}>
             <Link
               isExternal
-              fontSize={"22px"}
+              fontSize={"20px"}
               href="https://www.dal.ca/faculty/computerscience/news-events/news/2020/04/24/dal_students_takes_part_in_innovative_government_co_op_program.html?fbclid=IwAR0jFXrOUovqzjj2Tf3PFZsB1DyBI6BX0nJcm1cxX6YeFJV1eZNP0leHfp4"
             >
               Dal students takes part in innovative Government co‑op program
@@ -86,10 +86,10 @@ export default function Timeline() {
           </Text>
         </ListItem>{" "}
         {/* <ListItem>
-          <Text fontSize={"20px"}>
+          <Text fontSize={"18px"}>
             <Link
               isExternal
-              fontSize={"22px"}
+              fontSize={"20px"}
               href=""
             >
               <TITLE HERE>
